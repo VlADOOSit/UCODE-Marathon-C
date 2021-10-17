09f245d3a2b315555f7caa37089c0da491584700
Vlad Kharkovskiy <92023832+VlADOOSit@users.noreply.github.com>
2021-10-17 14:14:53 +0300

    Update mx_del_strarr.c

--- sprint07/t10/mx_del_strarr.c
+++ sprint07/t10/mx_del_strarr.c
@@ -3,11 +3,9 @@
 void mx_strdel(char **);
 
 void mx_del_strarr(char ***arr) {
-    if(*arr != NULL) {
-        for(int i = 0; *arr[i] != NULL ; i++) {
-            mx_strdel(&(*arr)[i]);
-        }
-    	free(*arr);
-        *arr = NULL;
+    for(int i = 0; (*arr)[i] != NULL; i++) {
+        mx_strdel(&((*arr)[i]));
     }
+    free(*arr);
+    *arr = NULL;
 }
